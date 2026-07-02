e04e859a9b00534e9c9cb9902981063791cdcd21
gana <ganakarsha@gmail.com>
2026-07-02 19:07:06 +0530

    not fully working database still have to get
    file_is
    uq_id

--- crud.py
+++ crud.py
@@ -1,36 +1,43 @@
 import aiogram
 from database import get_session_maker
 import database, sqlmodel, asyncio
-import pydantic
+import pydantic, Schema, gamdlUrl
+from typing import List
+
 async_session = get_session_maker()
 
-# Perfect sample data for testing your modern code
-sample_track_data = {
-    # Album table fields
-    "album_id": "alb_4k97x2Lp",
-    "album": "Short n' Sweet",
-    "artist": "Sabrina Carpenter",
 
-    # Track table fields
-    "id": "tr_1v8N0pQz",
-    "song_id": "song_espresso_01",
-    "title": "Espresso",
-    "url": "https://music.apple.com/us/album/espresso/1742230222?i=1742230223",
-    "file_id": "CQACAgIAAxkBAAEYv0lm23...H8uAAKyOAACvi7hS6vOa3f7QG80NgQ",
-    "file_unique_id": "AgADfQADu7u0Sg"
-}
-
-
-async def save_track_to_bot_db(track_data: dict):
+async def save_track_to_bot_db(track_lists: List[Schema.TrackInputSchema]):
     async with async_session() as session:
-
         async with session.begin():
-            album_obj = database.Albums(
-                album_id=track_data["album_id"],
-                album=sample_track_data["album"],
-                artist=sample_track_data["artist"]
-            )
-            await session.merge(album_obj)
+
+            for track_data in track_lists:
+                album_obj = database.Albums(
+                    album_id=track_data.album_id,
+                    album=track_data.album,
+                    artist=track_data.artist
+                )
+                await session.merge(album_obj)
+
+                track_obj = database.Tracks(
+                    artist=track_data.artist,
+                    album=track_data.album,
+                    album_id=track_data.album_id,
+                    song_id=track_data.song_id,
+                    file_id=None,
+                    file_unique_id=None,
+                    title=track_data.title,
+                    url=track_data.url
+                )
+                await session.merge(track_obj)
+
+async def main():
+    await database.init_db()
+    test = await gamdlUrl.get_any_url("https://music.apple.com/us/album/cigarettes-after-sex/1217977525")
+
+    await save_track_to_bot_db(test)
 
 
-asyncio.run(save_track_to_bot_db())
+if __name__ == "__main__":
+    # This runs your async lifecycle natively in Python
+    asyncio.run(main())
