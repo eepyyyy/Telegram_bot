353cb7686112bbdc9536233c97b0fff3d1629a15
gana <ganakarsha@gmail.com>
2026-07-02 19:11:28 +0530

    Reapply "not fully working database"
    This reverts commit 6b54dde297ce46ceac7ab8ed460cdc7e9d537947.

--- database.py
+++ database.py
@@ -15,10 +15,9 @@ class Albums(SQLModel, table=True):
 
 class Tracks(SQLModel, table=True):
     __tablename__ = "tracks"
-    id: str = Field(primary_key=True)
     file_id: Optional[str] = None
     file_unique_id: Optional[str] = None
-    song_id: Optional[str] = None
+    song_id: Optional[str] = Field(primary_key=True)
     title: Optional[str] = None
     album: Optional[str] = None
     artist: Optional[str] = None
@@ -38,50 +37,9 @@ def get_session_maker():
 
 async_session = get_session_maker()
 
-# Perfect sample data for testing your modern code
-sample_track_data = {
-    # Album table fields
-    "album_id": "alb_4k97x2Lp",
-    "album": "Short n' Sweet",
-    "artist": "Sabrina Carpenter",
-
-    # Track table fields
-    "id": "tr_1v8N0pQz",
-    "song_id": "song_espresso_01",
-    "title": "Espresso",
-    "url": "https://music.apple.com/us/album/espresso/1742230222?i=1742230223",
-    "file_id": "CQACAgIAAxkBAAEYv0lm23...H8uAAKyOAACvi7hS6vOa3f7QG80NgQ",
-    "file_unique_id": "AgADfQADu7u0Sg"
-}
-
-test = Schema.TrackInputSchema(
-    album_id="alb_4k97x2Lp",
-    album="Short n' Sweet",
-    artist="Sabrina Carpenter"
-)
-
-
-async def save_track_to_bot_db(track_data: Schema.TrackInputSchema):
-    async with async_session() as session:
-
-        async with session.begin():
-            album_obj = Albums(
-                album_id=track_data.album_id,
-                album=track_data.album,
-                artist=track_data.artist
-            )
-            await session.merge(album_obj)
-            track_obj = Tracks(
-                album_id=
-            )
 
 async def main():
     await init_db()
-
-    print("⏳ Saving test sample data...")
-    tests = Schema.TrackInputSchema(**sample_track_data)
-
-    await save_track_to_bot_db(test)
 
 
 if __name__ == "__main__":
