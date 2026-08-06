3ba78c29eb04465972330a724ce36489762d723e
eepyyyy <eepyyeepyyeepyy@gmail.com>
2026-08-06 13:52:42 +0530

    config: Update STREAM_SERVER_URL default domain to https://stream.eepy.in

--- server/config.py
+++ server/config.py
@@ -14,8 +14,9 @@ except ValueError:
     STORAGE_CHANNEL_ID = -1004423011255
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-STREAM_SERVER_URL = os.getenv("STREAM_SERVER_URL", "http://localhost:8088")
+STREAM_SERVER_URL = os.getenv("STREAM_SERVER_URL", "https://stream.eepy.in")
 
 SERVER_HOST = os.getenv("SERVER_HOST", "0.0.0.0")
 SERVER_PORT = int(os.getenv("SERVER_PORT", "8088"))
+
 
